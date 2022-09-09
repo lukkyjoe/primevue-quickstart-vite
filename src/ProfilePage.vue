c60dac18f1c487ff3133bba89cf2522fdb3a4c04
Joseph Luk <joseph.sc.luk@gmail.com>
2022-09-08 21:12:13 -0700

    Set fetched waste items correctly

--- src/ProfilePage.vue
+++ src/ProfilePage.vue
@@ -2,10 +2,15 @@
 import Autocomplete from 'primevue/autocomplete'
 import Editor from 'primevue/editor';
 import {ref, onMounted} from 'vue'
-const wasteItems = ref([])
+const wasteItems = ref([{name: 'foo'}])
+const filteredWasteItems = ref([])
 const value1 = ref('<div>Welcome to PrimeVue <b>Editor</b></div><div><br></div>');
 const selectedItem = ref()
-const searchItems = (event) => wasteItems.value = [{name: 'sup'}, {name: 'bar'}, {name: 'foo'}]
+const searchItems = (event) => {
+    console.log('event', event)
+    filteredWasteItems.value = [...wasteItems.value]
+
+}
 onMounted(() => {
     fetch('https://kzozb8le.directus.app/items/waste_items')
         .then(res => {
@@ -14,16 +19,16 @@ onMounted(() => {
         })
         .then(d => {
             console.log('d', d)
-            wasteItems.value = d.data.map(({name}) => name)
+            wasteItems.value = d.data
         })
 })
 </script>
 
 <template>
-    <Autocomplete v-model="selectedItem" :suggestions="wasteItems" :dropdown="true" @complete="searchItems" optionLabel="name">
-        <template #item="slotProps">
-            <div class="ml-2">hi {{slotProps.item.name}}</div>
+    <Autocomplete v-model="selectedItem" :suggestions="filteredWasteItems" :dropdown="true" @complete="searchItems" optionLabel="name" forceSelection>
+        <template #item="{item}">
+            <div class="ml-2">{{item.name}}</div>
         </template>
     </Autocomplete> 
-    <Editor v-model="value1"/>
+    <!-- <Editor v-model="value1"/> -->
 </template>
