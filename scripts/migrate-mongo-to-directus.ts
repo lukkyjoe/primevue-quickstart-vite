2564ebeb6d9e09c132cf368adf1fa1a4d76831a1
Joseph Luk <joseph.sc.luk@gmail.com>
2022-10-09 10:04:03 -0700

    Use await

--- scripts/migrate-mongo-to-directus.ts
+++ scripts/migrate-mongo-to-directus.ts
@@ -1,18 +1,22 @@
+import * as dotenv from "dotenv";
+dotenv.config();
 import { MongoClient } from "mongodb";
 
 // Connection URL
-const url = "mongodb://localhost:27017";
+console.log(process.env.MONGO_URL);
+// https://stackoverflow.com/a/52892398/3175120
+const url: string = process.env.MONGO_URL as string;
 const client = new MongoClient(url);
 
 // Database Name
-const dbName = "myProject";
+const dbName = "materially-api";
 
 async function main() {
   // Use connect method to connect to the server
   await client.connect();
   console.log("Connected successfully to server");
   const db = client.db(dbName);
-  const collection = db.collection("documents");
+  const collection = db.collection("things");
 
   // the following code examples can be pasted here...
   const findResult = await collection.find({}).toArray();
@@ -20,10 +24,18 @@ async function main() {
 
   // for each item: call api
 
-  return "done.";
+  return findResult;
 }
 
-main()
-  .then(console.log)
-  .catch(console.error)
-  .finally(() => client.close());
+async function run() {
+  try {
+    const result = await main();
+    console.log(result);
+  } catch (e) {
+    console.error(e);
+  } finally {
+    client.close();
+  }
+}
+
+run();
