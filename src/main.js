a183ed9fcbdb458dd3550eabc4f7b7fb1ac7d925
Joseph Luk <joseph.sc.luk@gmail.com>
2022-10-12 21:02:52 -0700

    Format

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import PrimeVue from 'primevue/config';
-import Button from 'primevue/button';
+import { createApp } from "vue";
+import App from "./App.vue";
+import PrimeVue from "primevue/config";
+import Button from "primevue/button";
 
-import 'primevue/resources/themes/saga-blue/theme.css';
-import 'primevue/resources/primevue.min.css';
-import 'primeicons/primeicons.css';          
+import "primevue/resources/themes/saga-blue/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
 
 const app = createApp(App);
 app.use(PrimeVue);
-app.component('Button', Button);
+app.component("Button", Button);
 
-app.mount('#app');
+app.mount("#app");
